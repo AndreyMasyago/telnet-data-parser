98e203fbadb0bd3fa1fa656a72deefc54bb3c356
Андрей Масяго <you@example.com>
2022-07-17 16:59:25 +0700

    Args and arg_parser added. Some minor fixes

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import socket
 import sys
-
-OUTPUT_FILE_NAME = "output.txt"
+import argparse
 
 
 def parse_msg(message):
@@ -16,7 +15,7 @@ def parse_msg(message):
     time = strings_chunks[2]
     time_parts = time.split('.')
     if len(time_parts) != 2:
-        raise ValueError('Incorrect value at millis')
+        raise ValueError('Incorrect value at time. Should be: HH:MM:SS.zhq')
 
     main_time = time_parts[0]
     millis = time_parts[1][:1]
@@ -28,36 +27,48 @@ def parse_msg(message):
 
 
 if __name__ == '__main__':
-    HOST = '127.0.0.1'
-    PORT = 23
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('--logfile', default='output.txt')
+    arg_parser.add_argument('--host', default='127.0.0.1')
+    arg_parser.add_argument('--port', default=23)
+    arg_parser.add_argument('--encoding', default='cp1251')
+    arg_parser.add_argument('--buffer_size', default=1024)
+
+    args = arg_parser.parse_args()
+
+    HOST = args.host
+    PORT = args.port
+    OUTPUT_FILE_NAME = args.logfile
+    ENCODING = args.encoding
+    BUFF_SIZE = args.buffer_size
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_address = (HOST, PORT)
 
-    print('starting up on %s port %s' % server_address)
+    # print('starting up on %s port %s' % server_address)
     sock.bind(server_address)
     sock.listen()
 
     while True:
-        print('waiting for a connection')
+        # print('waiting for a connection')
         connection, client_address = sock.accept()
-        print('connection from', client_address)
+        # print('connection from', client_address)
 
         with open(OUTPUT_FILE_NAME, 'a') as of:
 
             while True:
                 try:
-                    data = connection.recv(256)
+                    data = connection.recv(BUFF_SIZE)
                 except Exception as e:
                     print(e, file=sys.stderr)
                     break
 
-                print('received "%s"' % data)
+                # print('received "%s"' % data)
                 if data:
                     try:
                         # TODO: Unknown encoding from client. Add argument.
-                        decoded_data = data.decode('ascii')
-                        print('decoded data: "%s"' % decoded_data)
+                        decoded_data = data.decode(ENCODING)
+                        # print('decoded data: "%s"' % decoded_data)
 
                         group_number, fixed_time, channel_ID, member_number = parse_msg(decoded_data)
                         output_string = "Спортсмен, нагрудный номер {} прошёл отсечку {} в {}"\
@@ -72,7 +83,7 @@ if __name__ == '__main__':
                         print(e, file=sys.stderr)
 
                 else:
-                    print('no more data from', client_address)
+                    # print('no more data from', client_address)
                     break
 
         connection.close()
